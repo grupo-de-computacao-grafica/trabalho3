eb209faa693344a39a4eb817918dd897760bc0fb
João Pedro Abreu de Souza <jp_abreu@id.uff.br>
2018-09-09 12:22:41 -0300

    Update canvas.js

--- canvas.js
+++ canvas.js
@@ -1,1 +1,36 @@
+class Face{
+  
+  constructor(canvas,arestas){
+    self.canvas=canvas;
+    self.arestas=arestas;
+    
+  }
+  
+  desenhar(){
+    for(aresta in self.arestas){
+      aresta.desenhar(self.canvas);
+    }
+  }
+}
 
+class Aresta{
+  constructor(ponto1,ponto2)
+  {
+    self.ponto1=ponto1;
+    self.ponto2=ponto2;
+  }
+  
+  desenhar(canvas){
+    canvas.moveTo(ponto1.x,ponto1.y);
+    canvas.lineTo(ponto2.x,ponto2.y);
+    canvas.stroke();
+  }
+}
+
+class Ponto{
+  constructor(x,y)
+  {
+    self.x=x;
+    self.y=y;
+  }
+}
